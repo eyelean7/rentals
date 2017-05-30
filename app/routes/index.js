ed6babd7f5f5ccf8f07525cefe27aa4ffac536ab
Ilene Gorski and Jon Kang <ilene+jon@>
2017-05-30 10:43:04 -0700

    add all the stuff from homework

---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('rental');
+  },
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
---
+++ config/environment.js
@@ -0,0 +1,60 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'ig-rentals',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+
+    firebase: {
+      apiKey: "AIzaSyDpJ_cgLd3Zv7riftd8HAza3aUDHyMZUdU",
+      authDomain: "ig-rentals.firebaseapp.com",
+      databaseURL: "https://ig-rentals.firebaseio.com",
+      projectId: "ig-rentals",
+      storageBucket: "ig-rentals.appspot.com",
+      messagingSenderId: "283166641630"
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
